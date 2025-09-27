4a9f95dde39042ad557de6a90a29ca965988bcbb
TrshxRaccoon <trshxraccoon@gmail.com>
2025-09-27 17:39:12 +0530

    added logout functionality

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Home, CalendarCheck2, FileText, Megaphone } from 'lucide-react';
+import { Home, CalendarCheck2, FileText, Megaphone, LogOut } from 'lucide-react';
 import './Sidebar.css';
 
 export function Sidebar() {
   return (
     <nav className="sidebar">
-      <h2 />
-      <ul>
-        <li><NavLink to="/dashboard" end><Home /><span>Home</span></NavLink></li>
-        <li><NavLink to="/dashboard/attendance"><CalendarCheck2 /><span>Attendance</span></NavLink></li>
-        <li><NavLink to="/dashboard/assignments"><FileText /><span>Assignments</span></NavLink></li>
-        <li><NavLink to="/dashboard/announcements"><Megaphone /><span>Annoucements</span></NavLink></li>
-      </ul>
+      <div className="sidebar-content">
+        <h2 />
+        <ul>
+          <li><NavLink to="/dashboard" end><Home /><span>Home</span></NavLink></li>
+          <li><NavLink to="/dashboard/attendance"><CalendarCheck2 /><span>Attendance</span></NavLink></li>
+          <li><NavLink to="/dashboard/assignments"><FileText /><span>Assignments</span></NavLink></li>
+          <li><NavLink to="/dashboard/announcements"><Megaphone /><span>Annoucements</span></NavLink></li>
+          <li><NavLink to="/src/SignInPage.tsx"><LogOut /><span>Log Out</span></NavLink></li>
+        </ul>
+      </div>
     </nav>
   );
 }
